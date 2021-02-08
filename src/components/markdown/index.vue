aededc1f006fdacfb3a7af99a4348a0c1d83acc9
aaa <123@qq.com>
2021-02-08 11:10:44 +0800

    update

--- src/components/markdown/index.vue
+++ src/components/markdown/index.vue
@@ -44,7 +44,6 @@ export default {
     watch(
       () => proxy.$route,
       (router, prev) => {
-        list = [];
         state.code = router.query.id;
         state.authorList = [];
         proxy.$nextTick(() => {
